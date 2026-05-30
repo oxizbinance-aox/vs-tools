32a0c237ae0f4dca84c4decae787d53cf2ba8758
oxizbinance-aox <oxizbinance@gmail.com>
2026-05-30 14:22:28 +0700

    fix public assets serving

--- server.cjs
+++ server.cjs
@@ -44,6 +44,7 @@ const supabaseAdmin =
 app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true, limit: "500mb" }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/api/health", function (req, res) {
